37eaa1bc1c1eb0910a90c3a3be75c761dca258b6
agyesam12 <agyesam12@gmail.com>
2025-02-14 03:52:39 +0000

    added all the neccessary urls paths

--- authentication/urls.py
+++ authentication/urls.py
@@ -2,11 +2,27 @@ from django.urls import path
 from . import views
 from .views import *
 from django.contrib.auth import views as auth_view
+from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 
 urlpatterns = [
+    #apis urls
     path('', HomePage.as_view(), name='home'),
-    path('signup/', views.SignupView.as_view(), name='signup'),
+    path('signup/', UserSignupView.as_view(), name='user-signup'),
+    path('api_login/', LoginView.as_view(), name='token_obtain_pair'),
+    path("assign/", AssignDoctorView.as_view(), name="assign-doctor"),
+    path("my-doctors/", PatientDoctorsView.as_view(), name="patient-doctors"),
+    path("my-patients/", DoctorPatientsView.as_view(), name="doctor-patients"),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api_logout/', LogoutView.as_view(), name='api_logout'),
+    path('doctor-notes/submit/', SubmitDoctorNoteView.as_view(), name='submit_doctor_note'),
+    path('doctor-notes/retrieve/<int:patient_id>/', RetrieveDoctorNoteView.as_view(), name='retrieve_doctor_note'),
+    path("checklist/complete/", MarkChecklistCompletedView.as_view(), name="mark-checklist-completed"),
+    path("plan/complete/", MarkPlanTaskCompletedView.as_view(), name="mark-plan-completed"),
+    path("reminders/upcoming/", UpcomingRemindersView.as_view(), name="upcoming-reminders"),
+    #django class based view urls for testing my work
+    
     path('register/',UserSignUp.as_view(), name='register'),
     path('login/',LoginPageView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
