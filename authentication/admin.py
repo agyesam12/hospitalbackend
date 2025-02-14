47010091a95894baa673615bda2f47cb33c9c32c
agyesam12 <agyesam12@gmail.com>
2025-02-14 03:54:52 +0000

    registered my models in the admin dashboard

--- authentication/admin.py
+++ authentication/admin.py
@@ -4,4 +4,9 @@ from django.contrib.admin.models import LogEntry
 
 # Register your models here.
 admin.site.register(User),
-admin.site.register(LogEntry)
+admin.site.register(LogEntry),
+admin.site.register(DoctorAssignment),
+admin.site.register(DoctorNote),
+admin.site.register(ActionableStep),
+admin.site.register(PatientCheckIn),
+admin.site.register(Reminder),
