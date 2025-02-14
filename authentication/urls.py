df05e1e344dc6f80963224ae024b4a47de2b7229
agyesam12 <agyesam12@gmail.com>
2025-02-14 00:35:10 +0000

    added admin view all doctors view function and urls path

--- authentication/urls.py
+++ authentication/urls.py
@@ -12,5 +12,10 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
     path('profile/update/', ProfileUpdateView.as_view(), name='profile_update'),
     path('update_password/',auth_view.PasswordChangeView.as_view(template_name="update_password.html",success_url="user_dashboard" ),name='update_password'),
+    #admin urls
+    path('register/user/', AdminRegisterUsersView.as_view(), name='admin_create_user'),
+    path('users/', AdminUserListView.as_view(), name='admin_user_list'),
+    path('patients/',AdminPatientListView.as_view(), name= "patient_lists"),
+    path('doctors/',AdminDoctorListView.as_view(), name= "doctor_lists")
 ]
 
